f62b5f5bdf440d7747beb3fe1cccc99cdd2d38c1
Youssef-Touria <touria.youssef@laplateforme.io>
2026-01-02 17:41:58 +0100

    modification

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title>Autocomplétion</title>
+  <title>Autocompletion</title>
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
@@ -12,16 +12,6 @@
 <main class="home">
   <h1>Mon moteur de recherche</h1>
   <p>Commence à taper pour voir les suggestions.</p>
-
-  <form class="search-form" autocomplete="off" onsubmit="return false;">
-    <input
-      id="searchInput"
-      type="text"
-      placeholder="Rechercher..."
-      autocomplete="off"
-    >
-    <div id="suggestions"></div>
-  </form>
 </main>
 
 <script src="assets/js/app.js" defer></script>
